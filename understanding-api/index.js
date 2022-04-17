d49191d23642c0439a917cc59d9ae11586b5a098
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-04-17 23:55:24 +0300

    first phase of filter works

--- understanding-api/index.js
+++ understanding-api/index.js
@@ -1,9 +1,24 @@
 const allBtns = document.querySelectorAll("button");
+const allUserInfo = document.querySelectorAll("p");
 
 // Here, we are understanding which button is clicked,
 // and passing its id to the another func
 allBtns.forEach((button) => {
   button.addEventListener("click", (e) => {
-    console.log(e.target.id);
+    let filterId = e.target.id;
+    deneme2(filterId);
   });
 });
+
+function deneme2(filterId) {
+  allUserInfo.forEach((e) => {
+    if (e.dataset.id === filterId) {
+      console.log("asdasd");
+    }
+  });
+}
+deneme2();
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((value) => value.json())
+  .then((usersData) => console.log(usersData));
