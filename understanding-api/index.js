dca82ae5a7b89f5da6722f0d59e188f107127435
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-04-17 22:38:10 +0300

    before fetch, customizing

--- understanding-api/index.js
+++ understanding-api/index.js
@@ -1,5 +1,7 @@
 const allBtns = document.querySelectorAll("button");
 
+// Here, we are understanding which button is clicked,
+// and passing its id to the another func
 allBtns.forEach((button) => {
   button.addEventListener("click", (e) => {
     console.log(e.target.id);
