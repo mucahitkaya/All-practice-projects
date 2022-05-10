62c9f63350fb56e6d55e5f560b7fe062a9807f7c
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-05-10 21:28:52 +0300

    asda

--- 15projects-freecodecamp/stoicism/app.js
+++ 15projects-freecodecamp/stoicism/app.js
@@ -3,27 +3,22 @@ const personAbout = document.getElementById("person-about");
 const btns = document.querySelectorAll(".btns");
 
 let clickedBtn = "def";
+
 //When page loaded,callback(below) will work,and it will get local json file with fetch
-window.addEventListener("DOMContentLoaded", function () {
-  fetch("users.json")
-    .then((res) => res.json())
-    .then((result) => {
-      //array for api dataü
-      let persons = result;
-      personChanger(result);
-      //   console.log(persons[1].name);
-    });
-});
+
+fetch("users.json")
+  .then((res) => res.json())
+  .then((result) => {
+    //Array for api data
+    console.log(typeof result);
+    //   console.log(persons[1].name);
+  });
+
+//Tells us which btn is clicked.
 btns.forEach((element) => {
   element.addEventListener("click", (element) => {
-    // console.log(element.pageX); // Xdeki konumunu alıyoruz bölece
+    // console.log(element.pageX); || Xdeki konumunu alıyoruz bölece
     clickedBtn = element.target.id;
-    personChanger(clickedBtn);
   });
 });
-
 // fonk. içine paslanan değerlere arguments denir
-
-function personChanger(clickedBtn, persons) {
-  console.log(persons, clickedBtn);
-}
