5b7c9be10eb037e71c41bde2ba940c345c5a0e77
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-05-10 11:42:52 +0300

    needs control mech.

--- 15projects-freecodecamp/stoicism/app.js
+++ 15projects-freecodecamp/stoicism/app.js
@@ -1,5 +1,6 @@
 const personYear = document.getElementById("person-year");
 const personAbout = document.getElementById("person-about");
+const btns = document.querySelectorAll(".btns");
 
 //When page loaded,callback(below) will work,and it will get local json file with fetch
 window.addEventListener("DOMContentLoaded", function () {
@@ -8,6 +9,17 @@ window.addEventListener("DOMContentLoaded", function () {
     .then((result) => {
       //array for api dataü
       let persons = result;
-      console.log(persons[1].name);
+      personChanger(result);
+      //   console.log(persons[1].name);
     });
 });
+btns.forEach((element) => {
+  element.addEventListener("click", (element) => {
+    // console.log(element.pageX); // Xdeki konumunu alıyoruz bölece
+    personChanger(element.target.id);
+  });
+});
+
+function personChanger(persons) {
+  console.log(persons);
+}
