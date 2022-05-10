7da626bd817db69955a13b9793aef3493bdc5b63
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-05-10 11:23:09 +0300

    fetch used for local json file

--- 15projects-freecodecamp/stoicism/app.js
+++ 15projects-freecodecamp/stoicism/app.js
@@ -1,1 +1,13 @@
-console.log("aa");
+const personYear = document.getElementById("person-year");
+const personAbout = document.getElementById("person-about");
+
+//When page loaded,callback(below) will work,and it will get local json file with fetch
+window.addEventListener("DOMContentLoaded", function () {
+  fetch("users.json")
+    .then((res) => res.json())
+    .then((result) => {
+      //array for api dataü
+      let persons = result;
+      console.log(persons[1].name);
+    });
+});
