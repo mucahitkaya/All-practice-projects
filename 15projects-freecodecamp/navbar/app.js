ecc4432fb811e1b62453b2e2caa04fc4d398397d
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-05-11 16:21:08 +0300

    add remove and toggle understood

--- 15projects-freecodecamp/navbar/app.js
+++ 15projects-freecodecamp/navbar/app.js
@@ -3,5 +3,15 @@ const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
-  console.log(links.classList.contains()); // contains obje içinde parantez içinde aradığın şeyi sorgular true false döner
+  console.log(links.classList.contains("random")); // contains obje içinde parantez içinde aradığın şeyi sorgular true false döner
+  //yukardıaki kod classların içindeki aranan kelimeyi sorgular
+
+  // BURASI TOGGLE İŞLEVİ GÖREN KODDUR ADD VVE REMOVE KULLANINMINI BURDAN GÖREBİLİRSİN
+  //   if (links.classList.contains("show-links")) {
+  //     links.classList.remove("show-links");
+  //   } else {
+  //     links.classList.add("show-links");
+  //   }
+
+  links.classList.toggle("show-links");
 });
