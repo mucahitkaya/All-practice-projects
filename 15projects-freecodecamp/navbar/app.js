a8f3c032996e9022c78d5a5e328e0c77fe24e1bc
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-05-11 16:07:43 +0300

    some good sht

---
+++ 15projects-freecodecamp/navbar/app.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector(".nav-toggle");
+
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  console.log(links.classList.contains()); // contains obje içinde parantez içinde aradığın şeyi sorgular true false döner
+});
