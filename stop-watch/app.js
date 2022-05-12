fe6318aa17095f99f7b8791c239f845568e8aba1
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-05-12 17:52:06 +0300

    nice little stopwatch overhere

---
+++ stop-watch/app.js
@@ -0,0 +1,48 @@
+const timeElement = document.querySelector(".watch .time");
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+
+let seconds = 0;
+let interval = null;
+
+//Event listeners
+
+startBtn.addEventListener("click", start);
+stopBtn.addEventListener("click", stopp);
+resetBtn.addEventListener("click", reset);
+
+//Update the timer
+function timer() {
+  seconds++;
+
+  //Format our time
+  let hours = Math.floor(seconds / 3600);
+  let mins = Math.floor((seconds - hours * 3600) / 60);
+  let secs = seconds % 60;
+
+  if (secs < 10) secs = `0` + secs;
+  if (mins < 10) mins = `0` + mins;
+  if (hours < 10) hours = `0` + hours;
+
+  timeElement.innerHTML = `${hours}:${mins}:${secs}`;
+}
+
+function start() {
+  if (interval) {
+    return;
+  }
+
+  interval = setInterval(timer, 1000);
+}
+
+function stopp() {
+  clearInterval(interval);
+  interval = null;
+}
+
+function reset() {
+  stopp();
+  seconds = 0;
+  timeElement.innerHTML = `00:00:00`;
+}
