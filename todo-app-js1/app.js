fbd09c6102461f16328cad419d92b0334e3ce022
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-05-13 22:17:44 +0300

    working on js

---
+++ todo-app-js1/app.js
@@ -0,0 +1,46 @@
+window.addEventListener("load", () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const listElement = document.querySelector("#tasks");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(); //this will stops the refreshing page
+
+    const task = input.value;
+
+    if (!task) {
+      alert("please fill out the task");
+      return;
+    }
+
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("task");
+
+    //creates dom nodes
+    const taskContentElement = document.createElement("div");
+    taskContentElement.classList.add("content");
+
+    taskElement.appendChild(taskContentElement);
+
+    const taskInputElement = document.createElement("input");
+    taskInputElement.classList.add("text");
+    taskInputElement.type = "text";
+    taskInputElement.value = task;
+    taskInputElement.setAttribute("readonly", "readonly");
+
+    taskContentElement.appendChild(taskInputElement);
+
+    const taskActionElement = document.createElement("div");
+    taskActionElement.classList.add("actions");
+
+    const taskEditElement = document.createElement("button");
+    taskEditElement.classList.add("edit");
+    taskEditElement.innerHTML = "Edit";
+
+    const taskDeleteElement = document.createElement("button");
+    taskDeleteElement.classList.add("delete");
+    taskDeleteElement.innerHTML = "DELETE";
+
+    listElement.appendChild(taskElement);
+  });
+});
